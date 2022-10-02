196908a9e13ff237aac4d86ce5b31453d0d4a5b0
Liam <liam.oyh@gmail.com>
2022-10-02 22:54:41 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,22 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
-});
+}); */
 
-app.use("/public", express.static(__dir + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  //response = res.json({ message: "Hello json" });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
 module.exports = app;
