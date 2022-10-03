7ebc00057928b520bfd41663ada7d31c0bb67936
Liam <liam.oyh@gmail.com>
2022-10-03 00:24:22 +0000

    Update myApp.js and package-lock.json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " " + "-" + " " + req.ip);
+  next();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
